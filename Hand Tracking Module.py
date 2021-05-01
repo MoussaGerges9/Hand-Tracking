8340d96b20d942e324836102f8394db653fe6d41
Moussa Gerges <moussagerges9@gmail.com>
2021-05-01 21:57:13 +0200

    Finishing the module

--- Hand Tracking Module.py
+++ Hand Tracking Module.py
@@ -17,24 +17,28 @@ class handDetector():
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms,
                                                self.mpHands.HAND_CONNECTIONS)  # Design lines on the hand connecting points
         return img
 
-    # def findPosition(self, img, handNo, draw=True):
-    #     lmlist = []
-    #     for id, lm in enumerate(handLms.landmark):
-    #         h, w, c = img.shape
-    #         cx, cy = int(lm.x * w), int(lm.y * h)
-    #         print(id, cx, cy)
-    #         # if id == 5:  # Id of the finger
-    #         # cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED) # design circle on the selected point
-    #     return lmlist
+    def findPosition(self, img, handNo=0, draw=True):
+        lmlist = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmlist.append([id, cx, cy])
+                # if id == 5:  # Id of the point
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)  # design circle on the selected point
+        return lmlist
 
 
 def main():
@@ -45,6 +49,10 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+
+        if (len(lmList) != 0):
+            print(lmList[5])
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
